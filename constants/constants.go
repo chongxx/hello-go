2b72e6e0979166912bcfa4b3d6ab1778c422f473
chongxx <choongzhaoo@foxmail.com>
2017-12-07 14:57:25 +0800

    Constant variable define

---
+++ constants/constants.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+const pi = 3.14159
+
+func main() {
+	const Language string = "English"
+	const Truth = true
+
+	fmt.Println("Hello", pi)
+	fmt.Println("Hello", Truth)
+	fmt.Println("Hello", Language)
+
+}
