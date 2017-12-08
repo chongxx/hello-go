79281fe9e8cef8aa820dc83507d828f619020b67
chongxx <choongzhaoo@foxmail.com>
2017-12-08 20:28:26 +0800

    Add comment

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -7,7 +7,7 @@ func main() {
 	i, j := 1024, 2017
 
 	p := &i         // & 符号生成一个指向其对象的指针
-	fmt.Println(*p) // 通过pointer读取 i
+	fmt.Println(*p) // 通过pointer读取 i, *加指针局可以访问到指针指向的底层的值
 	*p = 2048       // 通过指针设置 i 的值
 	fmt.Println(i)
 
