328d672b5dff0a62f6c16c6fc585f9e52d12bdc8
chongxx <choongzhaoo@foxmail.com>
2017-12-15 21:29:17 +0800

    Implied Interface

--- interfaces/interfaces.go
+++ interfaces/interfaces.go
@@ -6,23 +6,12 @@ package main
 import (
 	"fmt"
 	"math"
+	"os"
 )
 
 type Abser interface {
 	Abs() float64
 	Up() int // MyFloat 和 Vertex 都没实现Up method 所以运行都会报错
-}
-
-func main() {
-	var a Abser
-	f := MyFloat(-math.Sqrt2)
-	v := Vertex{3, 4}
-
-	a = f
-	a = v
-
-	fmt.Println(a.Abs())
-
 }
 
 type MyFloat float64
@@ -41,4 +30,39 @@ type Vertex struct {
 
 func (v Vertex) Abs() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+// 隐式接口，类型通过显示接口的方法来实现接口，没有显式声明的必要，so not hava keyword 'implements'
+// 隐式接口解耦了实现接口的包和定义接口的包互不依赖
+
+// 包 io 定义了 Reader 和 Writer；其实不一定要这么做？
+
+type Reader interface {
+	Read(b []byte) (n int, err error)
+}
+
+type Writer interface {
+	Write(b []byte) (n int, err error)
+}
+
+type ReadWrite interface {
+	Reader
+	Writer
+}
+
+func main() {
+	//	var a Abser
+	//	f := MyFloat(-math.Sqrt2)
+	//	v := Vertex{3, 4}
+	//
+	//	a = f
+	//	a = v
+	//
+	//	fmt.Println(a.Abs())
+	var w Writer
+
+	// os.Stdout 实现了 Writer
+	w = os.Stdout
+
+	fmt.Fprint(w, "Hello, writer\n")
 }
